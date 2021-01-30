1d33157ccc716e78f9bcf786ad995e02e5c2d7d0
Remy Oudemans <remyoudemans@gmail.com>
2021-01-30 14:51:13 +0000

    feat: set up sudoku grid

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,49 @@
 <script>
-	export let name;
+  const rows = Array(9).fill(Array(9).fill(0));
+
+  const getRowClassName = rowIndex =>
+    (rowIndex + 1) % 3 === 0 ? 'border-bottom' : '';
+
+  const getCellClassName = cellIndex =>
+    (cellIndex + 1) % 3 === 0 ? 'border-right' : '';
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  <div class='grid'>
+    {#each rows as row, rowIndex}
+      <div class={getRowClassName(rowIndex)}>
+      {#each row as cell, cellIndex}
+        <input type='text' class={`cell ${getCellClassName(cellIndex)}`} />
+      {/each}
+      </div>
+    {/each}
+  </div>
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  main{
+    text-align: center;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+  .grid {
+    display: inline-block;
+    border: 2px solid black;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  .cell {
+    display: inline-block;
+    height: 2rem;
+    width: 2rem;
+    border: 1px solid black;
+    margin: 0;
+    text-align: center;
+  }
+
+  .border-bottom {
+    border-bottom: 2px solid black;
+  }
+
+  .border-right {
+    border-right: 2px solid black;
+  }
 </style>
