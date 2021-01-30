8656b678b47952607eab85018dd432bb3a2d5da1
Remy Oudemans <remyoudemans@gmail.com>
2021-01-30 18:03:33 +0000

    feat: only allow numbers in inputs

--- src/App.svelte
+++ src/App.svelte
@@ -1,19 +1,37 @@
 <script>
-  const rows = Array(9).fill(Array(9).fill(0));
+  let rows = Array(9).fill(Array(9).fill(''));
 
   const getRowClassName = rowIndex =>
     (rowIndex + 1) % 3 === 0 ? 'border-bottom' : '';
 
   const getCellClassName = cellIndex =>
     (cellIndex + 1) % 3 === 0 ? 'border-right' : '';
+
+  const onChange = (value, rowIndex, cellIndex, e) => {
+    console.log('called')
+
+    if (value.length <= 1 && '123456789'.includes(value)) {
+      rows[rowIndex] = rows[rowIndex].map((cell, ci) => ci === cellIndex ? value : cell)
+    }
+
+    e.currentTarget.value = rows[rowIndex][cellIndex]; // weird svelte hack to keep control
+  }
+
+  $: console.log(rows)
+
 </script>
 
 <main>
   <div class='grid'>
-    {#each rows as row, rowIndex}
+    {#each rows as row, rowIndex (rowIndex)}
       <div class={getRowClassName(rowIndex)}>
       {#each row as cell, cellIndex}
-        <input type='text' class={`cell ${getCellClassName(cellIndex)}`} />
+        <input
+          type='text'
+          class={`cell ${getCellClassName(cellIndex)}`}
+          value={cell}
+          on:input={e => onChange(e.currentTarget.value, rowIndex, cellIndex, e)}
+        />
       {/each}
       </div>
     {/each}
